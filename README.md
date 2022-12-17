82364490606f26ddf2a3e6fb41710d0ea46eee3e
DomanTom07 <tamasdoman7@citromail.hu>
2022-12-17 16:11:02 +0100

    Kezdés

---
+++ README.md
@@ -0,0 +1,7 @@
+# Rombitak (Dolgozat)
+
+A program bekéri egy rombusz oldalát és alfa szögét, ezekből kiszámolja a kerületet, és a területet.
+
+* Tamás Domán
+* Szoft II N
+* 2022-12-17
---
+++ src/views/MainWindow.java
@@ -0,0 +1,18 @@
+package views;
+
+public class MainWindow {
+    public MainWindow() {
+        initComponents();
+        addComponentsToWindow();
+        initWindow();
+    }
+    private void initComponents() {
+
+    }
+    private void addComponentsToWindow() {
+
+    }
+    private void initWindow() {
+
+    }
+}
